9f8dc88e5f57ceeb14b4f291d7b0471589dc9936
Dmitriy Semenov <dmitriy.semenov@tpv-tech.com>
2015-10-26 16:28:24 +0300

    Solution for MapLike

--- Week 4/MapLike.hs
+++ Week 4/MapLike.hs
@@ -1,7 +1,10 @@
 module MapLike where
 
+{- Ниже приведено определение класса MapLike типов, похожих на тип Map. Определите instance MapLike для типа ListMap, определенного ниже как список пар ключ-значение. Для каждого ключа должно храниться не больше одного значения. Функция insert заменяет старое значение новым, если ключ уже содержался в структуре. -}
+
 import Prelude hiding (lookup)
 import qualified Data.List as L
+import Debug.Trace
 
 class MapLike m where
     empty :: m k v
@@ -17,9 +20,27 @@ newtype ListMap k v = ListMap { getListMap :: [(k,v)] }
     
 instance MapLike ListMap where
     empty = ListMap []
-    lookup _ (ListMap []) = Nothing
-    lookup k' (ListMap ((k,v) : ms)) = if k' == k then Just v else lookup k ms
-    insert k' v' (ListMap []) = ListMap [(k', v')]
-    insert k' v' (ListMap ((k,v) : ms)) = if k' == k then ListMap ((k',v') : ms) else ListMap ((k, v) : insert k' v' ms)
-    delete _ [] = []
-    delete k' ((k,v):ms) = if k' == k then ms else (k,v) : delete k' ms
+    
+    lookup _  (ListMap []) = Nothing
+    lookup k' (ListMap ((k, v) : ms)) = if k' == k then Just v else lookup k' $ ListMap ms
+    
+    insert k v (ListMap kvs) = ListMap $ insert' k v kvs where
+    	insert' k' v' [] = [(k', v')]
+    	insert' k' v' ((k, v) : kvs) = if k' == k then (k', v') : kvs else (k, v) : insert' k' v' kvs
+    
+    delete k (ListMap kvs) = ListMap $ delete' k kvs where
+    	delete' _  [] = []
+    	delete' k' ((k, v) : kvs) = if k' == k then kvs else (k, v) : delete' k' kvs
+
+testMap1 = ListMap [(1, 'A'), (2, 'B'), (3, 'C')]
+testMap2 = ListMap [(1, 'A'), (2, 'Z'), (3, 'C')]
+
+type Tests = [Bool]
+tests		:: Tests
+tests =
+	[ lookup 2 testMap1 		== Just 'B'
+	, insert 2 'Z' testMap1 	== testMap2
+	]
+
+main = print tests
+
