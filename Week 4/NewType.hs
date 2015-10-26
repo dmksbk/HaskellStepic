08209f872154875837c2c19b682b27f60f906c55
Dmitriy Semenov <dmitriy.semenov@tpv-tech.com>
2015-10-26 14:52:23 +0300

    Added solution for newtype keyword

--- Week 4/NewType.hs
+++ Week 4/NewType.hs
@@ -1,12 +1,12 @@
 module NewType where
 
-newtype A a b = A b
-	newtype A = A
-	newtype A = A A A
-	newtype A = A a
-	newtype A a b = A a b
-	newtype A a = A
-	newtype A = A A
-	newtype A a b = A a
-	newtype A a = A a a
-	newtype A a = A a
+-- newtype A a b = A b 		-- Good
+-- newtype A = A 			-- Bad: The constructor of a newtype must have exactly one field but ‘A’ has none
+-- newtype A = A A A 		-- Bad: The constructor of a newtype must have exactly one field but ‘A’ has two
+-- newtype A = A a 			-- Bad: Not in scope: type variable ‘a’
+-- newtype A a b = A a b 	-- Bad: The constructor of a newtype must have exactly one field but ‘A’ has two
+-- newtype A a = A 			-- Bad: The constructor of a newtype must have exactly one field but ‘A’ has none
+-- newtype A = A A 			-- Good
+-- newtype A a b = A a 		-- Good
+-- newtype A a = A a a 		-- The constructor of a newtype must have exactly one field but ‘A’ has two
+-- newtype A a = A a 		-- Good
