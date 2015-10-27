3c3f3f236a0323a4a0bdaa5208386ba15d8e7ee3
Dmitriy Semenov <dmitriy.semenov@tpv-tech.com>
2015-10-27 09:40:24 +0300

    Added solution for MapFunctor in week 5

---
+++ Week 5/MapFunctor.hs
@@ -0,0 +1,23 @@
+module MapFunctor where
+
+{-
+Определите представителя класса Functor для типов данных Entry и Map. Тип Map представляет словарь, ключами которого являются пары:
+
+В результате должно обеспечиваться следующее поведение: fmap применяет функцию к значениям в словаре, не изменяя при этом ключи.
+
+GHCi> fmap (map toUpper) $ Map []
+Map []
+
+GHCi> fmap (map toUpper) $ Map [Entry (0, 0) "origin", Entry (800, 0) "right corner"]
+Map [Entry (0,0) "ORIGIN",Entry (800,0) "RIGHT CORNER"]
+-}
+
+data Entry k1 k2 v = Entry (k1, k2) v  deriving Show
+data Map k1 k2 v = Map [Entry k1 k2 v]  deriving Show
+
+instance Functor (Entry k1 k2) where
+    fmap f (Entry (k1, k2) v) = Entry (k1, k2) $ f v
+
+instance Functor (Map k1 k2) where
+	-- fmap f (Map []) = Map []
+    fmap f (Map l) = Map $ map (fmap  f) l
