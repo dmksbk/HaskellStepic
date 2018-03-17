4d45d3cba10e2c0ecbcb7455de0385dd2ca64d96
Dmitriy Semenov <dmksbk@gmail.com>
2018-03-17 23:02:56 +0300

    Working on FoldableCmps

--- 2.2 Effects management/FoldableCmps.hs
+++ 2.2 Effects management/FoldableCmps.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TypeOperators #-}
 
-import           Data.Monoid
-import           MyTest
+module Main where
+
+-- import           Data.Monoid
+-- import           MyTest
 
 -- Сделайте тип (|.|) представителем класса типов Foldable при условии, что аргументы композиции являются представителями Foldable.
 
@@ -9,10 +11,15 @@ infixr 9 |.|
 newtype (|.|) f g a = Cmps { getCmps :: f (g a) }  deriving (Eq,Show)
 
 instance (Functor f, Functor g) => Functor (f |.| g) where
-  fmap
+  fmap u = Cmps . fmap (fmap u) . getCmps
+
+instance (Applicative f, Applicative g) => Applicative (f |.| g) where
+  pure = Cmps . pure . pure
+  Cmps u <*> Cmps x = Cmps $ fmap (<*>) u <*> x
 
 instance (Foldable f, Foldable g) => Foldable (f |.| g) where
   -- foldMap :: (Monoid m, Foldable t) => (a -> m) -> t a -> m
+  -- foldMap u (Cmps x) = foldMap (foldMap u) u
   foldMap = undefined
 
 -- tests1 =
@@ -21,4 +28,6 @@ instance (Foldable f, Foldable g) => Foldable (f |.| g) where
 --   ]
 
 -- main = sequence_ tests1
-main = putStrLn . maximum . Cmps $ [Nothing, Just 2, Just 3]
+main :: IO ()
+main = putStrLn "Hallo world"
+  -- putStrLn . maximum . Cmps $ [Nothing, Just 2, Just 3]
--- 2.2 Effects management/TreeFold.hs
+++ 2.2 Effects management/TreeFold.hs
@@ -49,8 +49,8 @@ getLevels bs = roots : getLevels sibls where
 
 newtype Levelorder a = LevelO (Tree a)    deriving (Eq, Show)
 instance Foldable Levelorder where
-  foldr _ ini (LevelO Nil)              = ini
-  foldr f ini (LevelO b@(Branch _ _ _)) = foldr f ini . concat . getLevels $ [b]
+  foldr _ ini (LevelO Nil)           = ini
+  foldr f ini (LevelO b@(Branch {})) = foldr f ini . concat . getLevels $ [b]
 
 triv = Branch (Branch Nil 'l' Nil) 'c' (Branch Nil 'r' Nil)
 tree = Branch (Branch Nil 1 (Branch Nil 2 Nil)) 3 (Branch Nil 4 Nil)
@@ -62,17 +62,17 @@ tests1 =
   , lbl "   2"
   , putStrLn ""
   , lbl "Foldable DFS Tree lcr"
-  , (foldr (:) [] triv)          =?= "lcr"
-  , (foldr (:) [] tree)           =?= [1,2,3,4]
+  , foldr (:) [] triv          =?= "lcr"
+  , foldr (:) [] tree           =?= [1,2,3,4]
   , lbl "Foldable Preorder DFS tree clr"
-  , (foldr (:) [] $ PreO triv)    =?= "clr"
-  , (foldr (:) [] $ PreO tree)    =?= [3,1,2,4]
+  , foldr (:) [] $ PreO triv    =?= "clr"
+  , foldr (:) [] $ PreO tree    =?= [3,1,2,4]
   , lbl "Foldable Postorder DFS tree lrc"
-  , (foldr (:) [] $ PostO triv)    =?= "lrc"
-  , (foldr (:) [] $ PostO tree)   =?= [2,1,4,3]
+  , foldr (:) [] $ PostO triv    =?= "lrc"
+  , foldr (:) [] $ PostO tree   =?= [2,1,4,3]
   , lbl "Foldable LevelO BFS tree clr"
-  , (foldr (:) [] $ LevelO triv)    =?= "clr"
-  , (foldr (:) [] $ LevelO tree)  =?= [3,1,4,2]
+  , foldr (:) [] $ LevelO triv    =?= "clr"
+  , foldr (:) [] $ LevelO tree  =?= [3,1,4,2]
   ]
 
-main = sequence_ $ tests1
+main = sequence_ tests1
