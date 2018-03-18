2c3e239b0a6ea853bae0905628d37ff01d9fc1d0
Dmitriy Semenov <dmksbk@gmail.com>
2018-03-19 02:58:50 +0300

    small changes to TraversablePostOrderTree

--- 2.2 Effects management/TraversablePostOrderTree.hs
+++ 2.2 Effects management/TraversablePostOrderTree.hs
@@ -1,6 +1,6 @@
 module TraversablePostOrderTree where
 
-import           Control.Applicative (liftA3)
+import           Control.Applicative (liftA, liftA2, liftA3)
 import           Data.Traversable    (fmapDefault, foldMapDefault)
 
 -- Сделайте двоичное дерево
@@ -17,13 +17,15 @@ flip132 f a b c = f a c b
 
 instance Traversable Tree where
   -- sequenceA (Tr a b c) = Tr <$> a <*> b <*> c}
-  sequenceA Nil            = pure Nil
-  sequenceA (Branch l c r) = Branch <*> liftA3 flip132 <*> l <*> r <*> c
+  -- sequenceA Nil            = pure Nil
+  -- sequenceA (Branch l c r) = Branch <$> liftA3 flip <*> l <*> r <*> c
+  traverse _ Nil            = pure Nil
+  traverse f (Branch l c r) = Branch <$> traverse f l <*> liftA3 flip <*> traverse f r <*> f c
 
 -- представителем класса типов Traversable таким образом, чтобы обеспечить для foldMapDefault порядок обхода «postorder traversal»:
 testTree = Branch (Branch (Branch Nil 1 Nil) 2 (Branch Nil 3 Nil)) 4 (Branch Nil 5 Nil)
 -- GHCi> foldMapDefault (\x -> [x]) testTree =?= [1,3,2,5,4]
 
 main :: IO ()
-main = do
+main =
   print $ foldMapDefault (\x -> [x]) testTree
