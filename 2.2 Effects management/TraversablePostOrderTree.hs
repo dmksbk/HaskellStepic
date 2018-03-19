b2376f391a5bcefa41d3d43fde0e0d7a15367ca6
Dmitriy Semenov <dmitriy.semenov@tpv-tech.com>
2018-03-19 12:05:42 +0300

    Finally got solution for TraversablePreOrderTree

--- 2.2 Effects management/TraversablePostOrderTree.hs
+++ 2.2 Effects management/TraversablePostOrderTree.hs
@@ -16,11 +16,11 @@ flip132 :: (a -> b -> c -> d) -> a -> c -> b -> d
 flip132 f a b c = f a c b
 
 instance Traversable Tree where
-  -- sequenceA (Tr a b c) = Tr <$> a <*> b <*> c}
-  -- sequenceA Nil            = pure Nil
-  -- sequenceA (Branch l c r) = Branch <$> liftA3 flip <*> l <*> r <*> c
-  traverse _ Nil            = pure Nil
-  traverse f (Branch l c r) = Branch <$> traverse f l <*> liftA3 flip <*> traverse f r <*> f c
+   sequenceA Nil            = pure Nil
+   sequenceA (Branch l c r) = (flip . Branch) <$> sequenceA l <*> sequenceA r <*> c
+  --traverse _ Nil            = pure Nil
+  --traverse f (Branch l c r) = postOrderedTree <$> traverse f l <*> traverse f r <*> f c where
+  --  postOrderedTree l r  c = Branch l c r
 
 -- представителем класса типов Traversable таким образом, чтобы обеспечить для foldMapDefault порядок обхода «postorder traversal»:
 testTree = Branch (Branch (Branch Nil 1 Nil) 2 (Branch Nil 3 Nil)) 4 (Branch Nil 5 Nil)
