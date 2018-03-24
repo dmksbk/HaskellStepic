752e4ad74583a1f7a479a99e25dc7feefacb3440
Dmitriy Semenov <dmitriy.semenov@tpv-tech.com>
2018-03-24 13:48:13 +0300

    Started implementing Applicative and Monad for OddC

--- 2.2 Effects management/OddC.hs
+++ 2.2 Effects management/OddC.hs
@@ -24,24 +24,62 @@ cntInf = Bi 'A' 'B' cntInf
 -- Сделайте этот тип данных представителем классов типов Functor, Foldable и Traversable:
 
 instance Functor OddC where
+  --class Functor (f :: * -> *) where
+  --  fmap :: (a -> b) -> f a -> f b
   fmap f (Un x)        = Un (f x)
   fmap f (Bi x y rest) = Bi (f x) (f y) (fmap f rest)
 
 instance Foldable OddC where
+  --class Foldable (t :: * -> *) where
+  --  foldMap :: Monoid m => (a -> m) -> t a -> m
   foldMap f (Un x)        = f x
   foldMap f (Bi x y rest) = f x <> f y <> foldMap f rest
 
 toList  :: OddC a -> [a]
 toList = foldr (:) []
 
+instance Applicative OddC where
+  -- class Functor f => Applicative (f :: * -> *) where
+  --   pure :: a -> f a
+  --   (<*>) :: f (a -> b) -> f a -> f b
+  pure = Un
+  fx <*> vs = undefined
+
 instance Traversable OddC where
+  --class (Functor t, Foldable t) => Traversable (t :: * -> *) where
+  --  traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
   traverse f (Un x)        = Un <$> f x
   traverse f (Bi x y rest) = Bi <$> f x <*> f y <*> traverse f rest
+  --class (Functor t, Foldable t) => Traversable (t :: * -> *) where
+  --  sequenceA :: Applicative f => t (f a) -> f (t a)
+  sequenceA (Un x)        = pure x
+  sequenceA (Bi x y rest) = Bi <$> x <*> y <*> sequenceA rest
 
 -- GHCi> (+1) <$> cnt5                  == Bi 4 5 (Bi 2 3 (Un 43))
 -- GHCi> toList cnt5                    == [3,4,1,2,42]
 -- GHCi> sum cnt5                       == 52
 -- GHCi> traverse (\x->[x+2,x-2]) cnt1  == [Un 44,Un 40]
+
+instance Monad OddC where
+  -- class Applicative m => Monad (m :: * -> *) where
+  --   (>>=) :: m a -> (a -> m b) -> m b
+  --   (>>) :: m a -> m b -> m b
+  --   return :: a -> m a
+  return = pure
+  mv >>= k = undefined
+
+-------------------------------------------------
+-- Для типа данных OddC a (контейнер-последовательность, который по построению может содержать только нечетное число элементов) реализуйте функцию, конкатенирующую три таких контейнера в один:
+
+concat3OC :: OddC a -> OddC a -> OddC a -> OddC a
+concat3OC = undefined
+
+tst1 = Bi 'a' 'b' (Un 'c')
+tst2 = Bi 'd' 'e' (Bi 'f' 'g' (Un 'h'))
+tst3 = Bi 'i' 'j' (Un 'k')
+--GHCi> concat3OC tst1 tst2 tst3 =?= Bi 'a' 'b' (Bi 'c' 'd' (Bi 'e' 'f' (Bi 'g' 'h' (Bi 'i' 'j' (Un 'k')))))
+
+-- Обратите внимание, что соображения четности запрещают конкатенацию двух контейнеров OddC. Реализуйте всё «честно», не сводя к стандартным спискам.
 
 main :: IO ()
 main = do
