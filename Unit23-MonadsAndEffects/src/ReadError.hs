d97e0a0a4415568ed06282599a436e8432bda7a1
Dmitriy Semenov <dmksbk@gmail.com>
2018-04-08 17:25:49 +0300

    Solution for trySum

--- Unit23-MonadsAndEffects/src/ReadError.hs
+++ Unit23-MonadsAndEffects/src/ReadError.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module ReadError
     (
     ) where
 
+import           Control.Monad (zipWithM)
 import           Except
 import           MyTest
 
+-- | task 2.3.1.8
 -- Реализуйте функцию tryRead, получающую на вход строку и пытающуюся всю эту строку превратить в значение заданного типа. Функция должна возвращать ошибку в одном из двух случаев: если вход был пуст или если прочитать значение не удалось.
 
 -- Информация об ошибке хранится в специальном типе данных:
@@ -22,7 +26,8 @@ tryRead s  = if null r
 
 
 test1 =
-  [ runExcept (tryRead "5" :: Except ReadError Int)                 =?= Right 5
+  [ lbl "tryRead"
+  , runExcept (tryRead "5" :: Except ReadError Int)                 =?= Right 5
   , runExcept (tryRead "5" :: Except ReadError Double)              =?= Right 5.0
   , runExcept (tryRead "5zzz" :: Except ReadError Int)              =?= Left (NoParse "5zzz")
   , runExcept (tryRead "(True, ())" :: Except ReadError (Bool, ())) =?= Right (True,())
@@ -30,4 +35,28 @@ test1 =
   , runExcept (tryRead "wrong" :: Except ReadError (Bool, ()))      =?= Left (NoParse "wrong")
   ]
 
-tests = sequence_ test1
+-- end task 2.3.1.8
+
+-- | task 2.3.1.9
+
+-- Используя tryRead из прошлого задания, реализуйте функцию trySum, которая получает список чисел, записанных в виде строк, и суммирует их. В случае неудачи, функция должна возвращать информацию об ошибке вместе с номером элемента списка (нумерация с единицы), вызвавшим ошибку.
+
+-- Для хранения информации об ошибке и номере проблемного элемента используем новый тип данных:
+
+data SumError = SumError Int ReadError
+  deriving (Eq, Show)
+
+trySum :: [String] -> Except SumError Integer
+trySum xs = sum <$> zipWithM exc xs [1..] where
+  exc r n = withExcept (SumError n) (tryRead r)
+
+test2 =
+  [ lbl "trySum"
+  , runExcept (trySum ["10", "20", "30"])  =?= Right 60
+  , runExcept (trySum ["10", "20", ""])    =?= Left (SumError 3 EmptyInput)
+  , runExcept (trySum ["10", "two", "30"]) =?= Left (SumError 2 (NoParse "two"))
+  ]
+
+-- Подсказка: функция withExcept в этом задании может быть чрезвычайно полезна. Постарайтесь максимально эффективно применить знания, полученные на прошлой неделе.
+
+tests = sequence_ $ test1 ++ test2
